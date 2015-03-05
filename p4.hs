0e17076ade3c85af7e3a02eb1b61b7c453518237
Ophir LOJKINE <pere.jobs@gmail.com>
2015-03-05 08:59:45 +0100

    Q6

--- p4.hs
+++ p4.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 data Color = Red | Orange
 data Cell  = Empty | Full(Color)
 
@@ -6,13 +8,16 @@ instance Show Color where
   show Orange = " O "
 
 instance Show Cell where
-  show Empty   = "   "
+  show Empty   = " E "
   show (Full c) = show c
 
 type Column = [Cell]
 type Grid   = [Column]
 
+showGrid:: Grid -> String
+showGrid g = tail $ concatMap (('\n' :) . (concatMap show)) (transpose g)
+
 initial::Grid
 initial = replicate 7 (replicate 6 Empty)
 
-main = print (Empty)
+main = putStr $ showGrid initial
