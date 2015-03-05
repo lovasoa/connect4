9c8ffbf74dc685bbf80c4a0df5287ccced14d515
Ophir LOJKINE <pere.jobs@gmail.com>
2015-03-05 08:37:45 +0100

    Q5

--- p4.hs
+++ p4.hs
@@ -1,10 +1,18 @@
 data Color = Red | Orange
 data Cell  = Empty | Full(Color)
 
+instance Show Color where
+  show Red = " R "
+  show Orange = " O "
+
+instance Show Cell where
+  show Empty   = "   "
+  show (Full c) = show c
+
 type Column = [Cell]
 type Grid   = [Column]
 
 initial::Grid
 initial = replicate 7 (replicate 6 Empty)
 
-main = print "Hello"
+main = print (Empty)
