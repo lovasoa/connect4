adb9df939a27cf61adf1262545fe9bd8e969476f
Ophir LOJKINE <pere.jobs@gmail.com>
2015-03-05 08:14:52 +0100

    Question 4

--- p4.hs
+++ p4.hs
@@ -1,1 +1,10 @@
+data Color = Red | Orange
+data Cell  = Empty | Full(Color)
+
+type Column = [Cell]
+type Grid   = [Column]
+
+initial::Grid
+initial = replicate 7 (replicate 6 Empty)
+
 main = print "Hello"
